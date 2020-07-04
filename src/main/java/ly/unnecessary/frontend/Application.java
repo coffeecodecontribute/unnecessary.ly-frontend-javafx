b82a962025b63a9bcb14fcdb81f975249adb2e45
Felix Pojtinger <felix@pojtinger.com>
2020-07-04 14:57:15 +0200

    feat: Start adding basic structure

--- src/main/java/ly/unnecessary/frontend/Application.java
+++ src/main/java/ly/unnecessary/frontend/Application.java
@@ -1,13 +1,51 @@
 package ly.unnecessary.frontend;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        var scene = new Scene(new Label("Hello, world!"), 1080, 720);
+        var wrapper = new BorderPane();
+
+        var communitySwitcher = new VBox();
+
+        var communityList = new ScrollPane();
+
+        var communities = new VBox();
+        var community1 = new Button("1");
+        var community2 = new Button("2");
+        var community3 = new Button("3");
+        communities.getChildren().addAll(community1, community2, community3);
+
+        communityList.setContent(communities);
+
+        VBox.setVgrow(communityList, Priority.ALWAYS);
+
+        var communityAddButton = new Button("+");
+        var communityJoinButton = new Button("j");
+
+        var communityMainActions = new VBox(communityAddButton, communityJoinButton);
+
+        communitySwitcher.getChildren().addAll(communityList, communityMainActions);
+
+        var communityDetails = new VBox();
+
+        var communityContent = new HBox();
+
+        wrapper.setLeft(communitySwitcher);
+        wrapper.setCenter(communityContent);
+        wrapper.setRight(communityDetails);
+
+        var scene = new Scene(wrapper, 1080, 720);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("unnecessary.ly");
