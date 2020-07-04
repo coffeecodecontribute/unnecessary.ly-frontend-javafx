e373434a275032d838faab9d29602c90f461b6ca
LarsFlieger <lars.flieger2@gmail.com>
2020-07-04 15:03:04 +0200

    Updated physics and AssetLoading

--- src/main/java/ly/unnecessary/frontend/Application.java
+++ src/main/java/ly/unnecessary/frontend/Application.java
@@ -6,40 +6,27 @@ import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.components.IrremovableComponent;
-import com.almasb.fxgl.input.Input;
-import com.almasb.fxgl.physics.CollisionHandler;
-import com.almasb.fxgl.physics.HitBox;
 import javafx.geometry.Point2D;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import ly.unnecessary.frontend.components.BallComponent;
 
-import java.awt.*;
-import java.util.Arrays;
 import java.util.Map;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
+import static com.almasb.fxgl.dsl.FXGL.getAssetLoader;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.spawn;
 
 public class Application extends GameApplication {
 
 
-
     Entity ball, player;
 
-    static int ballSpeed = 18;
+    static int ballSpeed = 8;
     static int ballRadius = 24;
 
+    static final int collisionLogicSecurityPadding = 10;
+
     static int playerSpeed = 20;
-
-
-
-
-    Entity[] bricks = new Entity[42];
 
     @Override
     protected void initSettings(GameSettings gameSettings) {
@@ -51,9 +38,15 @@ public class Application extends GameApplication {
     }
 
     @Override
+    protected void onPreInit() {
+        getAssetLoader().loadSound("asset.wav");
+    }
+
+    @Override
     protected void initGameVars(Map<String, Object> vars) {
         vars.put("ballSpeed", ballSpeed);
         vars.put("playerSpeed", playerSpeed);
+        vars.put("ballRadius", ballRadius);
         vars.put("gameStatus", 1);
     }
 
@@ -75,20 +68,17 @@ public class Application extends GameApplication {
     protected void initGame() {
         getGameWorld().addEntityFactory(new GameEntityFactory());
 
-        int ballSpawnPointX = 0;
+        int ballSpawnPointX = getAppWidth() - 200;
         int ballSpawnPointY = getAppHeight() - 300;
 
         spawn("background", 0, 0);
         player = spawn("player", ballSpawnPointX, ballSpawnPointY + 75);
-        ball = spawn("ball", new SpawnData(ballSpawnPointX + 100, ballSpawnPointY)
-                .put("ballSpeed", ballSpeed)
-                .put("ballRadius", ballRadius)
-        );
+        ball = spawn("ball", new SpawnData(ballSpawnPointX + 100, ballSpawnPointY));
 
         int m = 0;
         for(int i = 0; i < 6; i++) {
             for(int j = 0; j < 7; j++) {
-                bricks[m++] = spawn("brick", 80 + i * 150, 60 + j * 50);
+                spawn("brick", 80 + i * 150, 60 + j * 50);
             }
         }
 
@@ -106,12 +96,14 @@ public class Application extends GameApplication {
         Point2D velocity = ball.getObject("velocity");
         ball.translate(velocity);
 
+        //System.out.println(Math.abs(velocity.getX()) + Math.abs(velocity.getY())); // TODO: Gets Ball Speed; Ball speed is to slow after first collide.
+
         if (ball.getY() < 0 || ball.getY() + ball.getWidth() > getAppHeight()) {
-            ball.setProperty("velocity", new Point2D(velocity.getX(), -velocity.getY()));
+            ball.getComponent(BallComponent.class).collide(new Point2D(velocity.getX(), -velocity.getY())); //TOP
         }
 
         if (ball.getX() < 0 || ball.getX() + ball.getHeight() > getAppWidth()) {
-            ball.setProperty("velocity", new Point2D(-velocity.getX(), velocity.getY()));
+            ball.getComponent(BallComponent.class).collide(new Point2D(-velocity.getX(), velocity.getY())); // RIGHT
         }
 
         //Game is lost
@@ -130,6 +122,12 @@ public class Application extends GameApplication {
     protected void initInput() {
         //onKey(KeyCode.D, "Move Right", () -> { player.getComponent(PlayerComponent.class).moveRight(); });
         //onKey(KeyCode.A, () -> player.getComponent(PlayerComponent.class).moveLeft());
+        onKey(KeyCode.L, () -> {
+            if(byType(EntityType.BRICK).isEmpty())
+                spawn("brick", getAppWidth()/2 - 250, 100);
+                spawn("point", byType(EntityType.BRICK).get(0).getX() - ball.getWidth() + collisionLogicSecurityPadding, 50);
+                spawn("point", byType(EntityType.BRICK).get(0).getX() + byType(EntityType.BRICK).get(0).getWidth() - collisionLogicSecurityPadding, 50);
+        });
     }
 
     @Override
@@ -141,12 +139,25 @@ public class Application extends GameApplication {
                 spawn("point", ball.getPosition());
             }
 
+            //play("asset.wav");
+
             brick.removeFromWorld();
-            ball.getComponent(BallComponent.class).collideBlock();
+
+            //ball.getComponent(BallComponent.class).collideBlock();
+
+
             Point2D velocity = ball.getObject("velocity");
-            ball.setProperty("velocity", new Point2D(velocity.getX(), -velocity.getY()));
+
+            if(ball.getX() > brick.getX() - ball.getWidth() + collisionLogicSecurityPadding && ball.getX() < brick.getX() + brick.getWidth() - collisionLogicSecurityPadding) {
+                ball.getComponent(BallComponent.class).collide(new Point2D(velocity.getX(), -velocity.getY()));
+                System.out.println("TOP");
+            }
+            else {
+                ball.getComponent(BallComponent.class).collide(new Point2D(-velocity.getX(), velocity.getY()));
+                System.out.println("RIGHT");
+            }
+
             //System.out.println("Brick X: " + brick.getX() + " | Brick Y: " + brick.getY());
-            System.out.println(Arrays.toString(bricks));
         });
 
         onCollisionBegin(EntityType.BALL, EntityType.PLAYER, (ball, player) -> {
@@ -175,7 +186,7 @@ public class Application extends GameApplication {
 
             //Ball collide logic
             Point2D velocity = ball.getObject("velocity");
-            ball.setProperty("velocity", new Point2D(ballSpeed * Math.sin(angle), -ballSpeed * Math.cos(angle)));
+            ball.getComponent(BallComponent.class).collide(new Point2D(ballSpeed * 1.5 * Math.sin(angle), -ballSpeed * 1.5 * Math.cos(angle)));
         });
         onCollisionEnd(EntityType.BALL, EntityType.PLAYER, (ball, player) -> {
             System.out.println("End Collision");
